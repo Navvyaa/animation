3f98e2f8a92cbdf0141fe9458f7db1a755f4e772
Navya Gupta <navyagupta171@gmail.com>
2024-10-08 19:30:20 +0530

    intro created

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const ctx = canvas.getContext('2d');
 var WIDTH = canvas.width = 900;
 var HEIGHT = canvas.height = 600;
 
-let gameStarted = false;
+var gameStarted = false;
 
 
 var colorArray= [
@@ -22,3 +22,25 @@ var lives = 3;
 var score = 0;
 var gameOver = false;
 
+startButton.addEventListener('click', startGame);
+resetButton.addEventListener('click', resetGame);
+
+function showIntro() {
+    introScreen.style.display = 'block';
+    gameScreen.style.display = 'none';
+}
+function startGame() {
+    introScreen.style.display = 'none';
+    gameScreen.style.display = 'flex';
+    gameStarted = true;
+    gameOver = false;
+    resetGame();
+    animate();
+}
+function resetGame(){
+    ctx.clearRect(0, 0, WIDTH, HEIGHT); 
+    gameStarted = false;
+    gameOver = false;
+}
+
+showIntro();

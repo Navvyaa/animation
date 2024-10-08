0bc4d5f3f4a85b620f90742947e02d98960e459c
Navya Gupta <navyagupta171@gmail.com>
2024-10-08 20:15:07 +0530

    Circle function created with draw,update and blast methods

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var HEIGHT = canvas.height = 600;
 var gameStarted = false;
 
 
-var colorArray= [
+var colorArray = [
     'blue',
     'green',
     'red'
@@ -37,8 +37,8 @@ function startGame() {
     resetGame();
     animate();
 }
-function resetGame(){
-    ctx.clearRect(0, 0, WIDTH, HEIGHT); 
+function resetGame() {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
     gameStarted = false;
     gameOver = false;
 }
@@ -59,6 +59,40 @@ function Circle(x, y, r, dx, dy, color) {
         ctx.fillStyle = this.color;
         ctx.fill();
     }
+    this.draw = function () {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
+        ctx.strokeStyle = 'black';
+        ctx.stroke();
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
 
+    this.update = function () {
+        if (this.x + this.r > WIDTH || this.x - this.r < 0) {
+            this.dx = -this.dx;
+        }
+        if (this.y + this.r > HEIGHT || this.y - this.r < 0) {
+            this.dy = -this.dy;
+        }
+        this.x += this.dx;
+        this.y += this.dy;
+        if (this.blasted) return;
+        if (mouse.x - this.x < 50 && mouse.x - this.x > -50 &&
+            mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+            if (this.r < maxRad) {
+                this.r += 1;
+            }
+        }
+        else if (this.r > minRad) {
+            this.r -= 1;
+        }
+        if (this.r >= maxRad) {
+            this.blasted = true;
+            this.blast();
+        }
+        this.draw();
+    }
+    
 }
 showIntro();
